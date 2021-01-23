5b0486c6706da7df16c8e6f1f24223e78b97faab
Omri Basch <omrib15@gmail.com>
2021-01-23 19:51:54 +0200

    default route

--- server/server.ts
+++ server/server.ts
@@ -1,2 +1,15 @@
-const omri: string = 'hi';
-console.log(omri);
+import {Request, Response} from 'express';
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+// route handler for the default home page
+app.get( "/", (req: Request, res: Response ) => {
+    res.send( "hey" );
+} );
+
+// start the express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
