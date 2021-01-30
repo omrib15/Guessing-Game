65b490786281eec195ae517465aa1b21917bc268
Omri Basch <omrib15@gmail.com>
2021-01-30 14:41:34 +0200

    initial routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreGameComponent } from './pages/pre-game/pre-game.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pre-game', component: PreGameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreGameComponent } from './pages/pre-game/pre-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreGameComponent
   ],
   imports: [
     BrowserModule,
